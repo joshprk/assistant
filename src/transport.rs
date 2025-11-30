581e0566807e01aead1429a6523c31b92db66c3a
Joshua Park <git@joshprk.me>
2025-11-30 14:30:04 -0500

    feat: remove abstract addr abstraction in transport layer

--- src/transport.rs
+++ src/transport.rs
@@ -4,92 +4,51 @@ use std::io::Write;
 use std::path::Path;
 use std::os::unix::net::UnixStream;
 
-use rand::Rng;
 use serde::Deserialize;
 use serde::Serialize;
-
-pub const SERVER_ADDR: u8 = 0;
-pub const BROADCAST_ADDR: u8 = 255;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type", content = "content")]
 pub enum TransportEvent {
-    ClientJoined { request_id: u64 },
-    ProvisionAddr { request_id: u64, new_addr: u8 },
-    Ping { },
+    ToolCall { name: String, arguments: String },
+    MessageDelta { delta: String },
 }
 
 #[derive(Debug)]
 pub struct TransportClient {
-    addr: u8,
-    stream: UnixStream,
+    stream: UnixStream
 }
 
 impl TransportClient {
     pub async fn connect(path: impl AsRef<Path>) -> anyhow::Result<Self> {
         let stream = UnixStream::connect(path)?;
-        let mut client = Self { addr: BROADCAST_ADDR, stream };
-
-        let provision_request_id = rand::rng().random_range(..u64::MAX);
-        let provision_request = TransportEvent::ClientJoined { request_id: provision_request_id };
-
-        client.send(0, provision_request).await?;
-
-        loop {
-            let event = client.recv().await?;
-
-            match event {
-                TransportEvent::ProvisionAddr { request_id, new_addr } => {
-                    if request_id == provision_request_id {
-                        client.addr = new_addr;
-                        return Ok(client)
-                    }
-                },
-                _ => continue,
-            };
-        }
+        Ok(Self { stream })
     }
 
-    pub fn connect_with_addr(path: impl AsRef<Path>, addr: u8) -> anyhow::Result<Self> {
-        let stream = UnixStream::connect(path)?;
-        Ok(Self { addr, stream })
+    pub fn from_stream(stream: UnixStream) -> anyhow::Result<Self> {
+        Ok(Self { stream })
     }
 
-    pub fn from_stream(stream: UnixStream, addr: u8) -> anyhow::Result<Self> {
-        Ok(Self { addr, stream })
-    }
-
-    pub async fn send(&mut self, recipient: u8, msg: TransportEvent) -> io::Result<()> {
+    pub async fn send(&mut self, msg: TransportEvent) -> io::Result<()> {
         let payload = serde_json::to_vec(&msg)?;
         let len_buf = (payload.len() as u32).to_be_bytes();
-        let recipient = recipient.to_be_bytes();
 
         self.stream.write_all(&len_buf)?;
-        self.stream.write_all(&recipient)?;
         self.stream.write_all(&payload)?;
 
         self.stream.flush()
     }
 
     pub async fn recv(&mut self) -> io::Result<TransportEvent> {
-        loop {
-            let mut len_buf = [0u8; 4];
-            let mut recipient_buf = [0u8; 1];
+        let mut len_buf = [0u8; 4];
 
-            self.stream.read_exact(&mut len_buf)?;
-            self.stream.read_exact(&mut recipient_buf)?;
+        self.stream.read_exact(&mut len_buf)?;
 
-            let len = u32::from_be_bytes(len_buf);
-            let recipient = recipient_buf[0];
+        let len = u32::from_be_bytes(len_buf);
+        let mut payload = vec![0; len as usize];
 
-            let mut buf = vec![0; len as usize];
+        self.stream.read_exact(&mut payload)?;
 
-            self.stream.read_exact(&mut buf)?;
-
-            if recipient == self.addr || recipient == BROADCAST_ADDR {
-                let event = serde_json::from_slice::<TransportEvent>(&buf)?;
-                return Ok(event)
-            }
-        }
+        Ok(serde_json::from_slice::<TransportEvent>(&payload)?)
     }
 }
